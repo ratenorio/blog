4556bed521d0a59ae01d3147595aed2a9278debe
raissa <r.tenorio@aluno.ifsp.edu.br>
2022-07-27 11:26:08 -0300

    blog

---
+++ sql/banco.sql
@@ -0,0 +1,22 @@
+create table usuario(
+    id int not null auto_increment,
+    nome varchar(50)  not null,
+    email varchar(255) not null,
+    senha varchar(60) not null,
+    data_criacao datetime not null default current_timestamp,
+    ativo tinyint not null default '0',
+    adm tinyint not null default '0',
+    PRIMARY KEY (id)
+);
+
+create table post (
+    id int not null auto_increment,
+    titulo varchar(255) not null,
+    texto text not null,
+    usuario_id int not null,
+    data_criacao datetime not null default current_timestamp,
+    data_postagem datetime not null,
+    PRIMARY KEY (id),
+    KEY fk_post_usuario_idx (usuario_id),
+    constraint fk_post_usuario foreign key (usuario_id) references usuario (id)
+);
