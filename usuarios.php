693591c6bbb94ad2418fa50bc07d9334e5caaced
raissa <r.tenorio@aluno.ifsp.edu.br>
2022-09-14 11:22:51 -0300

    blog

--- usuarios.php
+++ usuarios.php
@@ -28,7 +28,8 @@
                 <?php
                     require_once 'includes/funcoes.php';
                     require_once 'core/conexao_mysql.php';
-                    require_once 'core_mysql.php';
+                    require_once 'core/sql.php';
+                    require_once 'core/mysql.php';
 
                     foreach($_GET as $indice => $dado){
                         $$indice = limparDados($dado);
